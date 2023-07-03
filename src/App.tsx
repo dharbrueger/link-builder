1a042072824f0db9fd63beae92503b11d2496218
Daniel <dharbrueger@gmail.com>
2023-07-03 15:08:15 -0400

    added configurable collocation significance

--- src/App.tsx
+++ src/App.tsx
@@ -6,138 +6,169 @@ type TargetValue = string;
 type CollocationList = string[];
 
 type CollocationsResponse = {
-  id: number;
-  collocation: string;
-  relation: string;
-  pos: string;
-  significance: number;
-  basisword: string;
+	id: number;
+	collocation: string;
+	relation: string;
+	pos: string;
+	significance: number;
+	basisword: string;
+};
+
+type SliderProps = {
+  sliderValue: number;
+  handleSliderChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SliderComponent: React.FC<SliderProps> = ({ sliderValue, handleSliderChange}) => {
+	return (
+		<div style={{ marginTop: "50px" }}>
+			<input
+				type="range"
+				min="0"
+				max="10000"
+				value={sliderValue}
+				onChange={handleSliderChange}
+			/>
+			<div>Collocation Significance: {sliderValue}</div>
+		</div>
+	);
 };
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [potentialCollocations, setPotentialCollocations] =
-    useState<CollocationList>([]);
-  const [linkWords, setLinkWords] = useState<CollocationList>([]);
-  const activeLink = linkWords.length !== 0;
-  const impossibleLink = activeLink && potentialCollocations.length === 0;
+	const [query, setQuery] = useState("");
+	const [potentialCollocations, setPotentialCollocations] =
+		useState<CollocationList>([]);
+	const [linkWords, setLinkWords] = useState<CollocationList>([]);
+	const [sliderValue, setSliderValue] = useState(5000);
 
-  const extractPotentialCollocations = (
-    response: CollocationsResponse[]
-  ): string[] => {
-    const potentialCollocations: string[] = [];
+	const activeLink = linkWords.length !== 0;
+	const impossibleLink = activeLink && potentialCollocations.length === 0;
 
-    response.forEach((potentialCollocation) => {
-      const { basisword, collocation } = potentialCollocation;
-      const collocationWords = collocation.split(" ");
+	const extractPotentialCollocations = (
+		response: CollocationsResponse[]
+	): string[] => {
+		const potentialCollocations: string[] = [];
 
-      if (collocationWords[0] === basisword) {
-        potentialCollocations.push(collocationWords[1]);
-      }
-    });
+		response.forEach((potentialCollocation) => {
+			const { basisword, collocation } = potentialCollocation;
+			const collocationWords = collocation.split(" ");
 
-    return potentialCollocations;
-  };
+			if (collocationWords[0] === basisword) {
+				potentialCollocations.push(collocationWords[1]);
+			}
+		});
 
-  const getLinkableWords = async (
-    word = ""
-  ): Promise<CollocationsResponse | undefined> => {
-    if (!query && !word) return;
+		return potentialCollocations;
+	};
 
-    const options = {
-      method: "GET",
-      url: "https://linguatools-english-collocations.p.rapidapi.com/bolls/v2",
-      params: {
-        lang: "en",
-        query: word || query,
-        max_results: "25",
-        relation: "N:nn:N",
-        pos: "N",
-        min_sig: "180",
-      },
-      headers: {
-        "X-RapidAPI-Key": import.meta.env.VITE_LINGUA_TOOLS_KEY,
-        "X-RapidAPI-Host": "linguatools-english-collocations.p.rapidapi.com",
-      },
-    };
+	const getLinkableWords = async (
+		word = ""
+	): Promise<CollocationsResponse | undefined> => {
+		if (!query && !word) return;
 
-    try {
-      const response = await axios.request(options);
-      if (query && !activeLink) {
-        setLinkWords([query]);
-      }
+		const options = {
+			method: "GET",
+			url: "https://linguatools-english-collocations.p.rapidapi.com/bolls/v2",
+			params: {
+				lang: "en",
+				query: word || query,
+				max_results: "25",
+				relation: "N:nn:N",
+				pos: "N",
+				min_sig: sliderValue.toString(),
+			},
+			headers: {
+				"X-RapidAPI-Key": import.meta.env.VITE_LINGUA_TOOLS_KEY,
+				"X-RapidAPI-Host": "linguatools-english-collocations.p.rapidapi.com",
+			},
+		};
 
-      setPotentialCollocations(extractPotentialCollocations(response.data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+		try {
+			const response = await axios.request(options);
+			if (query && !activeLink) {
+				setLinkWords([query]);
+			}
 
-  const resetLink = () => {
-    setPotentialCollocations([]);
-    setLinkWords([]);
-  };
+			setPotentialCollocations(extractPotentialCollocations(response.data));
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const addLinkWord = (word: string) => {
-    setLinkWords([...linkWords, `- ${word}`]);
-    getLinkableWords(word);
-  };
+	const resetLink = () => {
+		setPotentialCollocations([]);
+		setLinkWords([]);
+	};
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value }: { value: TargetValue } = e.target;
-    setQuery(value);
-  };
+	const addLinkWord = (word: string) => {
+		setLinkWords([...linkWords, `- ${word}`]);
+		getLinkableWords(word);
+	};
 
-  return (
-    <>
-      <h1>li-nk builder</h1>
-      {activeLink &&
-        linkWords.map((word, index) => (
-          <span key={index} className="linkWord">
-            {word}
-          </span>
-        ))}
-      <div>
-        {!activeLink && !impossibleLink && (
-          <>
-            <input
-              type="text"
-              value={query}
-              onChange={handleInputChange}
-              placeholder="Enter a word"
-            />
-            <div style={{ marginTop: "50px" }}>
-              <button
-                onClick={() => getLinkableWords()}
-                disabled={!query}
-                style={{ marginRight: "20px" }}
-              >
-                Get Linkable Words
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-      <div className="card">
-        <ul>
-          {potentialCollocations.map((collocation, index) => (
-            <li
-              key={index}
-              onClick={() => addLinkWord(collocation)}
-              className="potentialCollocation"
-            >
-              {collocation}
-            </li>
-          ))}
-        </ul>
-      </div>
-      {activeLink && (
-          <div className="linkControls">
-            <button onClick={resetLink}>Reset Link</button>
-          </div>
-        )}
-    </>
-  );
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { value }: { value: TargetValue } = e.target;
+		setQuery(value);
+	};
+
+	const handleSliderChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { value }: { value: TargetValue } = e.target;
+		const parsedValue = parseInt(value);
+		setSliderValue(parsedValue);
+	};
+
+	return (
+		<>
+			<h1>li-nk builder</h1>
+			{activeLink &&
+				linkWords.map((word, index) => (
+					<span key={index} className="linkWord">
+						{word}
+					</span>
+				))}
+			<div>
+				{!activeLink && !impossibleLink && (
+					<>
+						<input
+							type="text"
+							value={query}
+							onChange={handleInputChange}
+							placeholder="Enter a word"
+						/>
+
+            <SliderComponent sliderValue={sliderValue} handleSliderChange={handleSliderChange}/>
+
+						<div style={{ marginTop: "50px" }}>
+							<button
+								onClick={() => getLinkableWords()}
+								disabled={!query}
+								style={{ marginRight: "20px" }}
+							>
+								Get Linkable Words
+							</button>
+						</div>
+					</>
+				)}
+			</div>
+			<div className="card">
+				<ul>
+					{potentialCollocations.map((collocation, index) => (
+						<li
+							key={index}
+							onClick={() => addLinkWord(collocation)}
+							className="potentialCollocation"
+						>
+							{collocation}
+						</li>
+					))}
+				</ul>
+			</div>
+			{activeLink && (
+				<div className="linkControls">
+					<button onClick={resetLink}>Reset Link</button>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default App;
